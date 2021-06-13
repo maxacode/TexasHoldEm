ea16e2d55b18eebb1f30fcc5b560221dd661c4ad
Max D - Mac <maxderevencha@gmail.com>
2021-06-13 17:14:58 -0400

    Upate

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ playersStillIn = []
 #bots bank account:
 allPlayersAccountValue = {}
 
+
+
 all_cards = [('Club', '1'), ('Club', '2'), ('Club', '3'), ('Club', '4'), ('Club', '6'), ('Club', '7'), ('Club', '8'), ('Club', '9'), ('Club', '10'), ('Club', 'J'), ('Club', 'Q'), ('Club', 'K'), ('Club', 'A'), ('Spade', '1'), ('Spade', '2'), ('Spade', '3'), ('Spade', '4'), ('Spade', '6'), ('Spade', '7'), ('Spade', '8'), ('Spade', '9'), ('Spade', '10'), ('Spade', 'J'), ('Spade', 'Q'), ('Spade', 'K'), ('Spade', 'A'), ('Heart', '1'), ('Heart', '2'), ('Heart', '3'), ('Heart', '4'), ('Heart', '6'), ('Heart', '7'), ('Heart', '8'), ('Heart', '9'), ('Heart', '10'), ('Heart', 'J'), ('Heart', 'Q'), ('Heart', 'K'), ('Heart', 'A'), ('Diamond', '1'), ('Diamond', '2'), ('Diamond', '3'), ('Diamond', '4'), ('Diamond', '6'), ('Diamond', '7'), ('Diamond', '8'), ('Diamond', '9'), ('Diamond', '10'), ('Diamond', 'J'), ('Diamond', 'Q'), ('Diamond', 'K'), ('Diamond', 'A')]
 
 def intro():
@@ -38,6 +40,7 @@ def intro():
 # Functions:
 # 1. Give out cards to everyone.
 def giveCards(players):
+    global playerDecks
     playerDecks = {}
     for playerNum in range(0,players+1):
 
@@ -51,6 +54,7 @@ def giveCards(players):
 # 2. Have players make their choices - Bots have a strategy they follow.
 def playerWage(playerDecks, players):
     pot = 0
+    global allPlayersWages
     #This rounds wages and what player did it:
     allPlayersWages = {}
     #mainPlayerWage = int(input("Enter your wage: "))
@@ -69,11 +73,19 @@ def playerWage(playerDecks, players):
         pot += botWage
     #print(allPlayersAccountValue)
     print(f"\nThe current pot is: {pot}\n")
-    mainPlayerActions(allPlayersWages)
+    checkPotEqual()
+
+def checkPotEqual():
+    print(f" all player wages = {allPlayersWages}")
+    for x in range(0,len(playersStillIn)):
+        highestBet = max(allPlayersWages, key=allPlayersWages.get)
+        if allPlayerWages[x]
 
 # Ask mainPlayer what htey want to do: Call, Raise, Fold, etc.
-def mainPlayerActions(allPlayersWages):
-    mainPlayerAction = input("Enter what action you would like to perform: 1:Fold, 2:Call, 3:Raise, 4:Stay:  " )
+def mainPlayerActions():
+   # mainPlayerAction = input("Enter what action you would like to perform: 1:Fold, 2:Call, 3:Raise, 4:Stay:  " )
+    mainPlayerAction = "4"
+
 
     if mainPlayerAction == "1":
         print("You are folding out! ")
@@ -84,22 +96,57 @@ def mainPlayerActions(allPlayersWages):
         highestBet = max(allPlayersWages, key=allPlayersWages.get)
         print(f"You are calling this amount {allPlayersWages[highestBet]} from Player {highestBet}")
         allPlayersWages[0] = allPlayersWages[highestBet]
-        callForBots(allPlayersWages)
+        callForBots(allPlayersWages, 0)
 
     elif mainPlayerAction == "3":
-        raiseAmount = int(input("What do you want to raise it to: "))
-
+        raiseAction(0)
     elif mainPlayerAction == "4":
         print("Staying" )
+        botActions()
+
+    else:
+        print("Some error in mainPlayerActions FUnc")
 
 
 #Actions for bot:
-def callForBots(allPlayerWages):
-    for player in range(1,playersStillIn +1):
-        randomChoice = random.randint(0,3)
+def callForBots(player):
+    highestBet = max(allPlayersWages, key=allPlayersWages.get)
+
+    print(f"Player{player} has called this much {highestBet}")
+    for x in range(1, len(playersStillIn)):
+        print(f"Player{player} has called this much {highestBet}")
+        allPlayersWages[x] = allPlayersWages[highestBet]
+        print(allPlayersWages)
+
+def botActions():
+    print(len(playersStillIn))
+    print(playerDecks)
+
+    # If bot has a pair of cards or suits
+    for x in range(1, len(playersStillIn)):
+        if playerDecks[x][1] == playerDecks[x][3] or playerDecks[x][0] == playerDecks[x][2]:
+            print(f"Player{x} has Pair of Cards or Suits")
+            raiseAction(x)
+    else:
+        round2()
+
+def raiseAction(player):
+    if player == 0:
+        raiseAmount = int(input("What would you like to raise by: "))
+        allPlayersWages[player] = raiseAmount
+        callForBots(player)
+
+    else:
+        print(allPlayersWages)
+        raiseAmount = allPlayersWages[player] * 1.1
+        allPlayersWages[player] = raiseAmount
+        print(allPlayersWages)
+        callForBots(player)
 
 # 3. Deal cards then send to func 2tf
 #
+def round2():
+    print("Starting Round 2")
 # 4. compare all hands to determine winner.
 #
 # 5. Give out rewards to winner/winners.
