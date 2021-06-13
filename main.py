fce5021515ace2c58d10edf3cb153c7dcd5b12dd
Max D - Mac <maxderevencha@gmail.com>
2021-06-12 22:13:35 -0400

    Upate

--- main.py
+++ main.py
@@ -4,19 +4,26 @@ import random
 
 #Players  bank.
 bank = 1000
+#Rounds in the game
 rounds = (1,2,3,4,5)
+#Players who have not folded.
 playersStillIn = []
+#bots bank account:
+allPlayersAccountValue = {}
 
 all_cards = [('Club', '1'), ('Club', '2'), ('Club', '3'), ('Club', '4'), ('Club', '6'), ('Club', '7'), ('Club', '8'), ('Club', '9'), ('Club', '10'), ('Club', 'J'), ('Club', 'Q'), ('Club', 'K'), ('Club', 'A'), ('Spade', '1'), ('Spade', '2'), ('Spade', '3'), ('Spade', '4'), ('Spade', '6'), ('Spade', '7'), ('Spade', '8'), ('Spade', '9'), ('Spade', '10'), ('Spade', 'J'), ('Spade', 'Q'), ('Spade', 'K'), ('Spade', 'A'), ('Heart', '1'), ('Heart', '2'), ('Heart', '3'), ('Heart', '4'), ('Heart', '6'), ('Heart', '7'), ('Heart', '8'), ('Heart', '9'), ('Heart', '10'), ('Heart', 'J'), ('Heart', 'Q'), ('Heart', 'K'), ('Heart', 'A'), ('Diamond', '1'), ('Diamond', '2'), ('Diamond', '3'), ('Diamond', '4'), ('Diamond', '6'), ('Diamond', '7'), ('Diamond', '8'), ('Diamond', '9'), ('Diamond', '10'), ('Diamond', 'J'), ('Diamond', 'Q'), ('Diamond', 'K'), ('Diamond', 'A')]
 
 def intro():
-    print(f"Welcome to Texas Hold Em Game - Offline Edition! \n You are starting with this much Dollars: {bank}")
+    global bank
+    #bank = int(input("Welcome to Texas Hold Em Game - Offline Edition! \n Enter $ for all players to start with (Default $10,000): "))
     #players = int(input("Enter how many bots you would like to play against: "))
     players = 3
 
     #Adding all players to the ones still playing.
     for x in range(0,players+1):
         playersStillIn.append(x)
+        allPlayersAccountValue[x] = bank
+    #print(allPlayersAccountValue)
     #print(playersStillIn)
     giveCards(players)
 
@@ -48,25 +55,30 @@ def playerWage(playerDecks, players):
     allPlayersWages = {}
     #mainPlayerWage = int(input("Enter your wage: "))
     mainPlayerWage = 100
+    allPlayersAccountValue[0] -= mainPlayerWage
+
     pot += mainPlayerWage
     print(f"Player 0 (You) is betting this much: {mainPlayerWage}")
     for playersWage in range(1,players + 1):
-        botWage = random.randint(0,bank)
+        botWage = random.randint(0,bank/10)
         print(f"Player {playersWage} is betting this much: {botWage}")
         allPlayersWages[playersWage] = botWage
+        allPlayersAccountValue[playersWage] -= botWage
+
         #print(allPlayersWages)
         pot += botWage
-
-    print(f"The current pot is: {pot}")
+    #print(allPlayersAccountValue)
+    print(f"\nThe current pot is: {pot}\n")
     mainPlayerActions(allPlayersWages)
 
 # Ask mainPlayer what htey want to do: Call, Raise, Fold, etc.
 def mainPlayerActions(allPlayersWages):
-    mainPlayerAction = input("Enter what action you would like to perform: 1:Fold, 2:Call, 3:Raise: " )
+    mainPlayerAction = input("Enter what action you would like to perform: 1:Fold, 2:Call, 3:Raise, 4:Stay:  " )
 
     if mainPlayerAction == "1":
         print("You are folding out! ")
-        playersStillIn.remove([0])
+       # playersStillIn.remove([0])
+        exit()
 
     elif mainPlayerAction == "2":
         highestBet = max(allPlayersWages, key=allPlayersWages.get)
@@ -74,9 +86,11 @@ def mainPlayerActions(allPlayersWages):
         allPlayersWages[0] = allPlayersWages[highestBet]
         callForBots(allPlayersWages)
 
-    elif mainPlayerAction == "Raise":
+    elif mainPlayerAction == "3":
         raiseAmount = int(input("What do you want to raise it to: "))
 
+    elif mainPlayerAction == "4":
+        print("Staying" )
 
 
 #Actions for bot:
@@ -84,7 +98,7 @@ def callForBots(allPlayerWages):
     for player in range(1,playersStillIn +1):
         randomChoice = random.randint(0,3)
 
-# 3. Deal cards then send to func 2
+# 3. Deal cards then send to func 2tf
 #
 # 4. compare all hands to determine winner.
 #
